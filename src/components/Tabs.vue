a0ccc7bc651cce45e8b4a0e0a707164baeef18e5
CrossScar <crossscarcs@gmail.com>
2024-08-18 09:44:45 -0700

    make tabs component declaritive

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const tab = ref(0);
+const { tabs } = defineProps<{
+  tabs: {
+    title: string,
+    sections: {
+      title: string,
+      body: string
+    }[]
+  }[]
+}>();
+
+const selectedTab = ref(0);
 </script>
 
 <template>
   <div class="tabs">
     <div class="tabTitles">
-      <span @click="tab = 0" :class="{selected: tab == 0}">Skills</span>
-      <span @click="tab = 1" :class="{selected: tab == 1}">Experience</span>
+      <span v-for="(tab, i) in tabs" @click="selectedTab = i" :class="{selected: selectedTab == i}">{{tab.title}}</span>
     </div>
-    <div class="content" v-show="tab == 0">
-      <h4>Visual Design</h4>
-      <p>Websites, infographics, videos, slideshows.</p>
-      <h4>Visual Conception</h4>
-      <p>I can easily think up objects or entire environments in 3D.</p>
-      <h4>Problem Solving</h4>
-      <p>Finding creative solutions to everyday problems.</p>
-    </div>
-    <div class="content" v-show="tab == 1">
-      <h4>2023 & 2024</h4>
-      <p>ACE Mentorship</p>
-      <h4>Spring 2022</h4>
-      <p>OSRO Soccer Referee</p>
-      <h4>Summer 2020</h4>
-      <p>Started a neighborhood bottle recycling service with a friend.</p>
+    <div v-for="(tab, i) in tabs" class="content" v-show="selectedTab == i">
+      <template v-for="section in tab.sections">
+        <h4>{{section.title}}</h4>
+        <p>{{section.body}}</p>
+      </template>
     </div>
   </div>
 </template>
